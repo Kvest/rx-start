df7b5be0c062eb99d28a02e4adfe1f9968652b09
Roman Bielokon <roman_slavyansk@mail.ru>
2016-04-20 15:16:59 +0300

    initial commit

---
+++ app/src/androidTest/java/rxstart/kvest/com/rxstart/ApplicationTest.java
@@ -0,0 +1,13 @@
+package rxstart.kvest.com.rxstart;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
